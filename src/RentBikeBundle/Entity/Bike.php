c643df17fef85f0e928ddf91fcc4ba4593f9a367
lrodriguezcabrales <lrodriguez@araujoysegovia.net>
2017-01-31 22:28:23 +0000

    entidades

---
+++ src/RentBikeBundle/Entity/Bike.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace RentBikeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bike
+ *
+ * @ORM\Table(name="bike")
+ * @ORM\Entity(repositoryClass="RentBikeBundle\Repository\BikeRepository")
+ */
+class Bike
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255, unique=true)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=255)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Bike
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     * @return Bike
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string 
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Bike
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
