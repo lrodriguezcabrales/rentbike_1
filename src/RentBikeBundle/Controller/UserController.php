41d56062732da53dccca87f9cac076601f9d976a
lrodriguezcabrales <lrodriguez@araujoysegovia.net>
2017-02-01 18:07:01 +0000

    backed

--- src/RentBikeBundle/Controller/UserController.php
+++ src/RentBikeBundle/Controller/UserController.php
@@ -3,7 +3,30 @@
 namespace RentBikeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
+
+	/**
+	 * Guardar usuario
+	 */
+	public function saveAction()
+	{
+		echo "entro aqui";
+		
+		$em = $this->get("doctrine")->getManager();
+
+        $request = $this->get('request');
+
+        $data = $request->getContent();
+        $data = json_decode($data, true);
+
+        print_r($data);
+
+        return new JsonResponse(array('success'=> false, 'message'=> 'jajaja'));
+
+	}
 }
